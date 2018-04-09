672812a1158e226fb45b74c7a3a7a30dd611d2d2
marcdledda <marcdl636@gmail.com>
2018-04-09 15:47:55 -0500

    added a script to the fly that destroys it when the player and the fly collides

---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyPickup : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		//If the Collider other is tagged with "Player"...
+		if (other.CompareTag("Player")) {
+			Destroy(gameObject);
+		}
+	}
+
+}
