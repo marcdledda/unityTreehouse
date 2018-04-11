2c501b6a9cb96396f791e2d1e824f58ddd761639
marcdledda <marcdl636@gmail.com>
2018-04-11 10:14:15 -0500

    add a random sound player script; haven't assign it to anything yet

---
+++ Assets/Scripts/RandomSoundPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundPlayer : MonoBehaviour {
+
+	private AudioSource audioSource;
+	[SerializeField]
+	private List<AudioClip> soundClips = new List<AudioClip>();
+	[SerializeField]
+	private float soundTimerDelay = 3f;
+	private float soundTimer;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Increment a time to count u to restarting
+		soundTimer = soundTimer + Time.deltaTime;
+		// If the timer reaches the delay...
+		if (soundTimer >= soundTimerDelay) {
+			//...reset the time...
+			soundTimer = 0f;
+			//...choose a random sound...
+			AudioClip randomSound = soundClips[Random.Range(0, soundClips.Count)];
+			audioSource.PlayOneShot(randomSound);
+		}
+	}
+}
